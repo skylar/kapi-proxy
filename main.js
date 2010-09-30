6b94e4dfaf72fcd237c68cc4ad431615f409bbc7
skylar <skylar@kiva.org>
2010-09-30 15:44:48 +0200

    Bind to custom port, all addresses.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var store = require('./dataStore');
 
 // CONSTANTS
 var refreshRate = 5*60*1000;
+var portNumber = 5482; // k-i-v-a
 
 // START crawler
 var onIndexUpdateComplete = function() {
@@ -19,6 +20,6 @@ crawler.updateIdIndex(onIndexUpdateComplete);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(json.stringify(store.getIds()));
-}).listen(8124, "127.0.0.1");
+}).listen(5482);
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running on port ' + portNumber);
